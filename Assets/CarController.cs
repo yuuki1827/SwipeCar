1427e472f0e96a29c943af13d3928d33a9ebc9b6
yuuki1827 <kouaka.hibatsuna@gmail.com>
2019-12-22 17:08:01 +0900

    Unityの勉強用

---
+++ Assets/CarController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+
+    // 車の初速を設定
+    float speed = 0;
+
+    Vector2 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // スワイプの長さを求める
+        if (Input.GetMouseButtonDown(0))
+        {
+            // マウスをクリックした座標
+            this.startPos = Input.mousePosition;
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            // マウスのクリックを離した座標
+            Vector2 endPos = Input.mousePosition;
+            float swipeLength = endPos.x - this.startPos.x;
+
+            // スワイプの長さを初速度に変換する
+            this.speed = swipeLength / 1000.0f;
+
+            // 効果音再生
+            GetComponent<AudioSource>().Play();
+        }
+
+        // 車の移動速度を設定
+        transform.Translate(this.speed, 0, 0);
+
+        // 車の減速処理
+        this.speed *= 0.98f;
+    }
+}
---
+++ Assets/GameDirector.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// UI部品を使用する為に必要
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour
+{
+
+    // ゲームオブジェクトに対応する変数を宣言
+    GameObject car;
+    GameObject flag;
+    GameObject distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.car = GameObject.Find("car");
+        this.flag = GameObject.Find("flag");
+        this.distance = GameObject.Find("Distance");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+         * 【car.transform.position.x】
+         * オブジェクト(car)にアタッチさせた
+         * Transformコンポーネントが持つ座標(position)の情報
+         */
+
+        // 車と旗との距離を計算（旗の座標 - 車の座標）
+        float length = this.flag.transform.position.x -
+            this.car.transform.position.x;
+
+        if(length >= 0)
+        {
+            this.distance.GetComponent<Text>().text =
+                "ゴールまで" + length.ToString("F2") + "m";
+        }
+        else
+        {
+            this.distance.GetComponent<Text>().text = "ゲームオーバー";
+        }
+
+        /*
+         * 【ToStringの書式】
+         *  例）D5
+         *      → Dは整数型、5は桁数
+         *  例) F2
+         *      → Fは固定小数点型、2は小数点以下の桁数
+         */
+    }
+}
